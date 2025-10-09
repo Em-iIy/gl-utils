d40f82c59e50ea3b5bf7d55a838ca989f3d4c6fc
Emily Winnink <emily.winnink@gmail.com>
2025-10-09 18:16:13 +0200

    wip more versatile framebuffer class (fix binding and unbinding)

--- inc/utils/FrameBuffer.hpp
+++ inc/utils/FrameBuffer.hpp
@@ -10,21 +10,75 @@ Created on: 16/01/2025
 #include "../../gl-utils.hpp"
 
 
+// class FrameBuffer {
+// 	public:
+// 		GLuint			id = {};
+// 		GLsizei 		width;
+// 		GLsizei 		height;
+// 		Tex2d			render_texture;
+// 		RenderBuffer	render_buffer;
+
+// 		FrameBuffer();
+// 		void	generate(GLsizei in_width, GLsizei in_height);
+// 		void	bind();
+// 		void	unbind();
+// 		void	del();
+
+// 		void	link_render_texture(GLenum attachment);
+// 		void	link_render_buffer(RenderBuffer &rbo);
+// 		bool	check_status();
+// };
+
 class FrameBuffer {
 	public:
-		GLuint			id = {};
-		GLsizei 		width;
-		GLsizei 		height;
-		Tex2d			render_texture;
-		RenderBuffer	render_buffer;
-
 		FrameBuffer();
-		void	generate(GLsizei in_width, GLsizei in_height);
-		void	bind();
-		void	unbind();
-		void	del();
+		~FrameBuffer();
+	
+		void				create(int width, int height);
+		void				destroy();
 
-		void	link_render_texture(GLenum attachment);
-		void	link_render_buffer(RenderBuffer &rbo);
-		bool	check_status();
+		void				bind();
+		static void			unbind();
+
+		void				attachColorTexture(
+			int index = 0,
+			GLenum internalFormat = GL_RGBA8,
+			GLenum format = GL_RGBA,
+			GLenum type = GL_UNSIGNED_BYTE,
+			bool nearest = true,
+			bool clamp = true,
+			bool mipmap = false
+		);
+
+		void				ensureDepthTexture(
+			GLenum internalFormat = GL_DEPTH_COMPONENT24
+		);
+
+		void				ensureDepthRbo(
+			GLenum internalFormat = GL_DEPTH24_STENCIL8
+		);
+
+		bool				checkStatus() const;
+
+		void				setDrawBuffers(const std::vector<GLenum> &drawBuffers);
+
+		void				blitDepthFrom(GLuint srcFbo, int srcWidth, int srcHeight);
+
+		void				clear(bool clearColor, bool clearDepth, const mlm::vec4 clearColorRGBA) const;
+
+		void				resize(int newWidth, int newHeight);
+
+		GLuint				getColorTexture(int index = 0) const;
+		GLuint				getDepthTexture() const;
+		GLuint				getId() const;
+		int					getWidth() const;
+		int					getHeight() const;
+
+	private:
+		GLuint				_id;
+		GLsizei				_width;
+		GLsizei				_height;
+		std::vector<GLuint>	_colorTextures;
+		GLuint				_depthTexture;
+		GLuint				_depthRbo;
 };
--- src/FrameBuffer.cpp
+++ src/FrameBuffer.cpp
@@ -5,64 +5,249 @@ Created on: 16/01/2025
 
 #include "FrameBuffer.hpp"
 
-FrameBuffer::FrameBuffer()
+FrameBuffer::FrameBuffer(): _id(0), _width(0), _height(0), _depthTexture(0), _depthRbo(0)
+{}
+
+FrameBuffer::~FrameBuffer()
 {
-	this->render_texture = Tex2d(GL_CLAMP_TO_BORDER, GL_LINEAR);
+	destroy();
 }
 
-void	FrameBuffer::generate(GLsizei in_width, GLsizei in_height)
+void	FrameBuffer::create(int width, int height)
 {
-	this->width = in_width;
-	this->height = in_height;
-	glGenFramebuffers(1, &this->id);
-	this->bind();
+	destroy();
 
-	this->render_texture.load_render_texture(width, height, GL_RGB, GL_UNSIGNED_BYTE);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this->render_texture.get_id(), 0);
+	_width = width;
+	_height = height;
+	glGenFramebuffers(1, &_id);
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
 
-	this->render_buffer.generate();
-	this->render_buffer.bind();
-	this->render_buffer.storage(GL_DEPTH24_STENCIL8, this->width, this->height);
-	this->render_buffer.unbind();
-	this->link_render_buffer(this->render_buffer);
-	if (!this->check_status())
-		throw std::runtime_error("Framebuffer incomplete");
+void	FrameBuffer::destroy()
+{
+	if (_id != 0)
+	{
+		glDeleteFramebuffers(1, &_id);
+		_id = 0;
+	}
+
+	for (GLuint texture : _colorTextures)
+		if (texture)
+			glDeleteTextures(1, &texture);
+	_colorTextures.clear();
+
+	if (_depthTexture != 0)
+	{
+		glDeleteTextures(1, &_depthTexture);
+		_depthTexture = 0;
+	}
+
+	if (_depthRbo != 0)
+	{
+		glDeleteRenderbuffers(1, &_depthRbo);
+		_depthRbo = 0;
+	}
 }
 
 void	FrameBuffer::bind()
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, this->id);
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
 }
 
 void	FrameBuffer::unbind()
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
 }
 
-void	FrameBuffer::del()
+void	FrameBuffer::attachColorTexture(int index, GLenum internalFormat, GLenum format, GLenum type, bool nearest, bool clamp, bool mipmap)
 {
-	this->render_buffer.del();
-	this->render_texture.del();
-	glDeleteFramebuffers(1, &this->id);
-}
+	if (_id == 0)
+		throw std::runtime_error("Framebuffer hasn't been created");
+	
+	if (static_cast<int>(_colorTextures.size()) <= index)
+		_colorTextures.resize(index + 1, 0);
+	
+	if (_colorTextures[index] == 0)
+		glGenTextures(1, &_colorTextures[index]);
+	glBindTexture(GL_TEXTURE_2D, _colorTextures[index]);
+	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, _width, _height, 0, format, type, nullptr);
 
-void	FrameBuffer::link_render_texture(GLenum attachment)
-{
-	glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, this->render_texture.get_id(), 0);
-}
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, nearest ? GL_NEAREST : GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, nearest ? GL_NEAREST : GL_LINEAR);
 
-void	FrameBuffer::link_render_buffer(RenderBuffer &rbo)
-{
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo.id);
-}
-
-bool	FrameBuffer::check_status()
-{
-
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	if (mipmap)
 	{
-		return (false);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, nearest ? GL_NEAREST_MIPMAP_NEAREST : GL_LINEAR_MIPMAP_LINEAR);
 	}
-	return (true);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + index, GL_TEXTURE_2D, _colorTextures[index], 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void	FrameBuffer::ensureDepthTexture(GLenum internalFormat)
+{
+	if (_id == 0)
+		throw std::runtime_error("Framebuffer hasn't been created");
+	
+	if (_depthRbo != 0)
+	{
+		glDeleteRenderbuffers(1, &_depthRbo);
+		_depthRbo = 0;
+	}
+	if (_depthTexture)
+		glGenTextures(1, &_depthTexture);
+	
+	glBindTexture(GL_TEXTURE_2D, _depthTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, _width, _height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, nullptr);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_NONE);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _depthTexture, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void	FrameBuffer::ensureDepthRbo(GLenum internalFormat)
+{
+	if (_id == 0)
+		throw std::runtime_error("Framebuffer hasn't been created");
+	
+	if (_depthTexture != 0)
+	{
+		glDeleteTextures(1, &_depthTexture);
+		_depthTexture = 0;
+	}
+	if (_depthRbo == 0)
+		glGenRenderbuffers(1, &_depthRbo);
+	
+	glBindRenderbuffer(GL_RENDERBUFFER, _depthRbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, _width, _height);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	if (internalFormat == GL_DEPTH24_STENCIL8)
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _depthRbo);
+	else
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthRbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+bool	FrameBuffer::checkStatus() const
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	if (status != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cerr << "Framebuffer not complete: 0x" << std::hex << status << std::endl;
+		return (false); 
+	}
+	return (true); 
+}
+
+void	FrameBuffer::setDrawBuffers(const std::vector<GLenum> &drawBuffers)
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	glDrawBuffers(static_cast<GLsizei>(drawBuffers.size()), drawBuffers.empty() ? nullptr : drawBuffers.data());
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void	FrameBuffer::blitDepthFrom(GLuint srcFbo, int srcWidth, int srcHeight)
+{
+	if (_id == 0)
+		throw std::runtime_error("Dst Framebuffer hasn't been created");
+	if (_depthTexture == 0 && _depthRbo == 0)
+		throw std::runtime_error("Dst Framebuffer has no depth attachment");
+
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, srcFbo);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _id);
+
+	glBlitFramebuffer(0, 0, srcWidth, srcHeight, 0, 0, _width, _height, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+	
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void	FrameBuffer::clear(bool clearColor, bool clearDepth, const mlm::vec4 clearColorRGBA) const
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, _id);
+	GLbitfield mask = 0;
+	if (clearColor)
+	{
+		glClearColor(clearColorRGBA.x, clearColorRGBA.y, clearColorRGBA.z, clearColorRGBA.w);
+		mask |= GL_COLOR_BUFFER_BIT;
+	}
+	if (clearDepth)
+		mask |= GL_DEPTH_BUFFER_BIT;
+	if (mask)
+		glClear(mask);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void	FrameBuffer::resize(int newWidth, int newHeight)
+{
+	if (newWidth == _width && newHeight == _height)
+		return ;
+	_width = newWidth;
+	_height = newHeight;
+
+	for (GLuint texture : _colorTextures)
+	{
+		if (texture != 0)
+		{
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		}
+	}
+
+	if (_depthTexture != 0)
+	{
+		glBindTexture(GL_TEXTURE_2D, _depthTexture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, _width, _height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, nullptr);
+	}
+
+	if (_depthRbo != 0)
+	{
+		glBindRenderbuffer(GL_RENDERBUFFER, _depthRbo);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, _width, _height);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+	}
+}
+
+GLuint	FrameBuffer::getColorTexture(int index) const
+{
+	if (index < 0 || index >= static_cast<int>(_colorTextures.size()))
+		return (0);
+	return (_colorTextures[index]);
+}
+
+GLuint	FrameBuffer::getDepthTexture() const
+{
+	return (_depthTexture);
+}
+
+GLuint	FrameBuffer::getId() const
+{
+	return (_id);
+}
+
+int	FrameBuffer::getWidth() const
+{
+	return (_width);
+}
+
+int	FrameBuffer::getHeight() const
+{
+	return (_height);
 }
